9e425371025015debf761164a50539882b05e7cb
Emma <110700240+ebreyaue@users.noreply.github.com>
2022-10-24 01:30:13 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+//create server with nodejs
+const express = require("express");
+const app = express();
+app.use(express.static(__dirname+'/public/'));
+app.listen('3000',function(){console.log('listen port 3000');});
+
